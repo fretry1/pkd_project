e57cc409c486a99329c0d722447fefbb69a510b9
Uegjo <jakob.jonsson.aberg@gmail.com>
2025-02-19 12:22:03 +0100

    prettier formating

--- src/lib/type.ts
+++ src/lib/type.ts
@@ -1,53 +1,50 @@
 export type Product = {
-    id          : number,
-    titel       : string,
-    description : string,
-    price       : number
-}
+	id: number;
+	titel: string;
+	description: string;
+	price: number;
+};
 
 export type Order = {
-    id : number,
-    created_at : number,
-    status : Status,
-    products : Product[],
-    total : number
-}
+	id: number;
+	created_at: number;
+	status: Status;
+	products: Product[];
+	total: number;
+};
 
 export type discount = {
-    code : string,
-    amount : number   
-}
+	code: string;
+	amount: number;
+};
 
 export type OrderItem = {
-    product : number,
-    quantity : number,
-    discountCode : string
-}
+	product: number;
+	quantity: number;
+	discountCode: string;
+};
 
 export type Status = keyof typeof STATUS;
 
 export const STATUS = {
-    PLACED : "PLACED",
-    PAYMENT_COMPLETE : "PAYMENT_COMPLETE",
-    SHIPPED : "SHIPPED",
-    IN_TRANSIT : "IN_TRANSIT",
-    DELIVERED : "DELIVIRED"
-}
+	PLACED: 'PLACED',
+	PAYMENT_COMPLETE: 'PAYMENT_COMPLETE',
+	SHIPPED: 'SHIPPED',
+	IN_TRANSIT: 'IN_TRANSIT',
+	DELIVERED: 'DELIVIRED'
+};
 
 export type Payment = {
-    amount : number,
-    transactionDate : string,   
-}
+	amount: number;
+	transactionDate: string;
+};
 
 export type CustomerDetails = {
-    SSN : string,
-    name: string,
-    cardNumber : string,
-    country : string,
-    city : string,
-    postalCode : string,
-    address : string
-}
-
-
-
+	SSN: string;
+	name: string;
+	cardNumber: string;
+	country: string;
+	city: string;
+	postalCode: string;
+	address: string;
+};
