08e70a170eb25831da5053f3fa783f833d5e02cf
Uegjo <jakob.jonsson.aberg@gmail.com>
2025-02-23 11:57:18 +0100

    first Go function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,14 +9,56 @@
 		})
 		console.log(res.status)
 		if (!res.ok) {
-			json = `failed: res ${res.status}`
+			json1 = `failed: res ${res.status}`
 			return
 		}
 		const obj = await res.json()
-		json = JSON.stringify(obj, null, 2)
+		json1 = JSON.stringify(obj, null, 2)
 	}
 
-	let json = $state('')
+	let json1 = $state('')
+
+    const getAllOrders = async () => {
+		let res = await fetch("http://localhost:8080/orders", {
+			method: "GET",
+			headers: {
+				'accept': 'application/json'
+			}
+		})
+		console.log(res.status)
+		if (!res.ok) {
+			json2 = `failed: res ${res.status}`
+			return
+		}
+		const obj = await res.json()
+		json2 = JSON.stringify(obj, null, 2)
+	}
+
+	let json2 = $state('')
+
+    let deleteInput = ""
+
+    const DeleteOrder = async () => {
+		let res = await fetch(`http://localhost:8080/orders/${deleteInput}`, {
+			method: "DELETE",
+			headers: {
+				'accept': 'application/json'
+			}
+		})
+		console.log(res.status)
+		if (!res.ok) {
+			json3 = `failed: res ${res.status}`
+			return
+		}
+		const obj = await res.json()
+		json3 = JSON.stringify(obj, null, 2)
+	}
+
+	let json3 = $state('')
+
+
+
+
 
 </script>
 
@@ -27,20 +69,110 @@
 	</div>
 </div>
 
-<button
-	onclick={() => createOrder()}
->
-	send fetch request
-</button>
 
-<textarea
-	bind:value={json}
-></textarea>
+
+
+
+
+
+
+<div>
+
+    <button
+	    onclick={() => createOrder()}
+    >
+	    create order
+    </button>
+
+    <textarea
+	bind:value={json1}
+    ></textarea>
+
+</div>
+
+<div>
+
+    <button
+	    onclick={() => getAllOrders()}
+    >
+	    get all orders
+    </button>
+
+    <textarea
+	bind:value={json2}
+    ></textarea>
+
+</div>
+
+<div>
+    <button
+
+	onclick={() => DeleteOrder()}
+    >
+	delete order
+    </button>
+    <input
+    bind:value={deleteInput}
+    />   
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 <style>
-  textarea {
-    min-height: 300px;
-    min-width: 400px;
-  }
-</style>
+    textarea {
+      min-height: 300px;
+      min-width: 400px;
+    }
+  </style>
+
+
+
+
+
+
