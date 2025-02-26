a4bc61a0735ba52352bede75a58bea81c4da00d7
Uegjo <jakob.jonsson.aberg@gmail.com>
2025-02-26 11:43:46 +0100

    order service uppdate

--- src/services/order_services.ts
+++ src/services/order_services.ts
@@ -7,12 +7,29 @@ import type {
 	CustomerDetails,
 	Receipt
 } from "$lib/type"
+import { apiCommunicator } from "./api-client"
 
-function create_empty_order() {}
+function create_empty_order(url: string,): Result{
+    //take irl return 
 
-function modify_order_products() {}
+}
 
-function modify_order_status() {}
+async function getOrder (orderID: string)  {
+    const [order, err] = await apiCommunicator.get<Order>(`/orders/${orderID}`) 
+    if(err) {
+    }
+
+    return order
+}
+
+async function modifyOrderProducts(url: string, orderID: number, productID: number){
+    const [order, err] = await apiCommunicator.put<Order>(`/orders/${orderID}/products/${productID}`) 
+    if(err) {        
+    }
+     
+}
+
+function modify_order_status(url: string, ) {}
 
 function remove_order() {}
 
