ac20b53d9ad9ea4d0d38957f58decf6b5891e04c
mac0002 <mac.0002.uu@gmail.com>
2025-03-07 18:48:57 +0100

    Filters from local

--- src/lib/state/product-store.svelte.ts
+++ src/lib/state/product-store.svelte.ts
@@ -3,6 +3,7 @@ import type { Product } from "$lib/type"
 import OrderService from "$lib/services/order-service"
 import ProductService from "$lib/services/product-service"
 import { OrderStore } from "$lib/state/order-store.svelte"
+import { preloadData } from "$app/navigation"
 
 export const PRODUCT_KEY = "products"
 
@@ -13,7 +14,41 @@ export class ProductStore {
 		this.products = products
 	}
 
-	sortProducts(by: "name" | "price", order: "asc" | "desc") {}
+	#sortByPrice(prod: Product[], mode?: "asc" | "desc"): Product[] {
+		return mode === "asc"
+			? prod.sort((a, b) => a.price - b.price)
+			: mode === "desc"
+				? prod.sort((a, b) => b.price - a.price)
+				: prod
+	}
+
+	#sortByAlph(prod: Product[], mode?: "asc" | "desc"): Product[] {
+		return mode === "asc"
+			? prod.sort((a, b) => (a.title > b.title ? 1 : b.title > a.title ? -1 : 0))
+			: mode === "desc"
+				? prod.sort((a, b) => (b.title > a.title ? 1 : a.title > b.title ? -1 : 0))
+				: prod
+	}
+
+	sortProducts(by: "name" | "price", order: "asc" | "desc"): Product[] {
+		switch (by) {
+			case "name":
+				switch (order) {
+					case "asc":
+						return this.#sortByAlph(this.products, "asc")
+
+					case "desc":
+						return this.#sortByAlph(this.products, "desc")
+				}
+			case "price":
+				switch (order) {
+					case "asc":
+						return this.#sortByPrice(this.products, "asc")
+					case "desc":
+						return this.#sortByPrice(this.products, "desc")
+				}
+		}
+	}
 }
 
 export function setProductStore() {
